22cd8538a204f7216d35d94d4b866cdc0ca84262
Dramirez1642 <daltonramirez00@gmail.com>
2024-03-25 23:10:31 -0700

    Completed exercise 2.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+let repeatedString = '';
 
-};
-
+const repeatString = function(phrase, number) {
+    repeatedString = ''
+    if (number < 0) {
+        repeatedString = 'ERROR';
+    }
+    else {
+        
+        for (let i = 0; i < number; i++) {
+            repeatedString += phrase;
+        }
+    }
+return repeatedString;
+}
 // Do not edit below this line
 module.exports = repeatString;
