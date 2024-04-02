85aeab968d964482e76c4d097e3afd2929baae6d
Dramirez1642 <daltonramirez00@gmail.com>
2024-04-01 23:44:29 -0700

    Completed exercise 5.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum = 0; 
+    if (a < 0 || b < 0) {
+        return "ERROR";
+    }
+    else if (typeof a != 'number' || typeof b != 'number') {
+        return "ERROR";
+    }
+    else {
+        if (a < b) {
+            for (i = a; i <= b; i++) {
+                sum += i;
+            }
+            return sum;
+        }
+        if (b < a) {
+            for (i = a; i >= b; i--) {
+                sum += i;
+            }
+            return sum;
+        }
+    } 
 };
 
 // Do not edit below this line
