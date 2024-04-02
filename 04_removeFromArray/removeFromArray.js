42c91c86484c6b4c72d72110624c2e4610bed863
Dramirez1642 <daltonramirez00@gmail.com>
2024-04-01 21:26:15 -0700

    Completed exercise 4.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ... elements) {
+    return array.filter(e => elements.indexOf(e) < 0);
 };
 
 // Do not edit below this line
