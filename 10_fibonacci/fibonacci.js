06efebd2c293c697f6c9b9b22470f8e3ec66475e
Dramirez1642 <daltonramirez00@gmail.com>
2024-08-07 22:45:36 -0700

    finished 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    let input
+    if (typeof input !== 'number') {
+        input = parseInt(a)
+    }
+    else {
+        input = a
+    }
 
+    if (input < 0) {return "OOPS"}
+
+    if (input == 0) {return 0;}
+
+    let secondPrev = 0
+    let firstPrev = 1
+
+    for (i = 2; i <= input; i++) {
+        let currentNum = secondPrev + firstPrev
+        secondPrev = firstPrev
+        firstPrev = currentNum
+    }
+    return firstPrev
 };
 
 // Do not edit below this line
