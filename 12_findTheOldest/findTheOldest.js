eba6aa492ec15b9574fb10aedb7f878f3e1536a0
Dramirez1642 <daltonramirez00@gmail.com>
2024-08-07 23:28:09 -0700

    finished 12_findtheoldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let currentYear = (new Date()).getFullYear()
+    console.log(currentYear)
+    people.sort(
+        function(a,b) {
+            let aCurrentAge = a.yearOfDeath ? a.yearOfDeath - a.yearOfBirth : currentYear - a.yearOfBirth
+            let bCurrentAge = b.yearOfDeath ? b.yearOfDeath - b.yearOfBirth : currentYear - b.yearOfBirth
+            return bCurrentAge - aCurrentAge
+        }
+        )
 
+    return people[0]
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
