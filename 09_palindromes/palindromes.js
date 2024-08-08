21c5f21d33a5d73cac799c9446d06191bd8a6c23
Dramirez1642 <daltonramirez00@gmail.com>
2024-08-07 22:17:59 -0700

    Finished 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+    let validChars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    let initString = words.toLowerCase().split('').filter(character => validChars.includes(character)).join('')
+    let finalString = initString.split('').reverse().join('')
+    return initString === finalString
 };
 
 // Do not edit below this line
