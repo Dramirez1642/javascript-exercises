de411abefec6fe4ae12546e757bf92a9eef0335d
Dramirez1642 <daltonramirez00@gmail.com>
2024-08-07 21:03:38 -0700

    Completed 08_calculator exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let totalSum = numbers.reduce((sum, current) => sum + current, 0);
+  return totalSum
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let totalMult = numbers.reduce((value, current) => value * current, 1)
+  return totalMult
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let value = 1
+
+  if (a === 0) {
+    return 1;
+  }
+    for (let i = a; i > 0; i--) {
+      value *= i
+    }
+    return value;
 };
 
 // Do not edit below this line
